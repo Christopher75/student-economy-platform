c8ff31c8d9b1381c2da0920aa5a196249f47fc97
Christopher75 <christopherishaku75@gmail.com>
2026-03-23 12:07:45 +0300

    Admin Account Script

--- build.sh
+++ build.sh
@@ -8,3 +8,17 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if one doesn't exist yet
+python manage.py shell -c "
+from accounts.models import CustomUser
+if not CustomUser.objects.filter(is_superuser=True).exists():
+    CustomUser.objects.create_superuser(
+        email='$ADMIN_EMAIL',
+        password='$ADMIN_PASSWORD',
+        full_name='Admin',
+    )
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+"
